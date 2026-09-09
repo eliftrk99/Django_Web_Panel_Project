c5831c0f31e294847def3f3b732d5ea1d9321d71
Elif TÜRK <yoruk_kezban@hotmail.com>
2026-09-09 17:14:41 +0300

    Updated settings.py to include new db configs.

--- panelapp/panelapp/settings.py
+++ panelapp/panelapp/settings.py
@@ -32,14 +32,13 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,testserver',
+    default='*',
     cast=Csv()
 )
 
-# CSRF ve Security ayarları
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
-    default='http://localhost:8000,http://127.0.0.1:8000,http://testserver',
+    default='http://localhost:3434,http://127.0.0.1:3434,http://127.0.0.1:8000,http://testserver,https://j76xbql9-3434.euw.devtunnels.ms',
     cast=Csv()
 )
 
